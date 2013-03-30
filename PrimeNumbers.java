7566a94386b4510903f306a0105433c397221426
Matthew Boyette <Dyndrilliac@gmail.com>
2013-03-30 00:27:21 -0400

    minor comment/readme updates, no code changes

--- PrimeNumbers.java
+++ PrimeNumbers.java
@@ -3,9 +3,10 @@
 	Author: Matthew Boyette
 	Date:   2/21/2012
 	
-	This application generates the first 'n' prime numbers and displays them to the user. The method used to test for primality is fairly
-	efficient but will naturally pale in comparison to industry standard libraries optimized for this task. This is merely proof of concept
-	code. It will also approximate the amount of time in seconds that it took to generate the list.
+	This application generates the first 'n' prime numbers and displays them to the user. The method used to test 
+	for primality is fairly efficient but will naturally pale in comparison to industry standard libraries optimized 
+	for this task. This is merely proof of concept code. It will also approximate the amount of time in seconds that 
+	it took to generate the list.
 */
 
 import api.gui.*;
